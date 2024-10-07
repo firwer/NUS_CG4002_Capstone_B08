e5209538ff20613663e9be3f263536e2897288c2
nichyjt <nicholas.yek@gmail.com>
2024-10-07 11:40:53 +0800

    add sample usage

--- int_comms/beetle/beetle.ino
+++ int_comms/beetle/beetle.ino
@@ -10,16 +10,63 @@ void setup() {
     pinMode(ledPin, OUTPUT); // Set the LED pin as an output
 }
 
+bool hasData(float probability){ // mock
+    float r = random(0, 1000) / 1000.0;
+    return r < probability;
+}
+
+void processGamestate(packet_gamestate_t state){ // mock
+    delay(state.bullet_num);
+}
+
 // This driver code is to simulate usage of the internal comms API
 void loop() {
     // Wait until the handshake is completed
-    while(!isConnected) {
-        await_handshake(false);
-    }
+    while(!ic_connect()) { }
     
-    // setLED(1);
+    // Mock the IMU sending 
+    long start_time = millis();
+    long delay = 25; // 40Hz == period of 25ms
+    packet_gamestate_t state;
     while(1){
-      communicate();
+        // For each loop in this FSM...
+
+        // 1. Send reliable if there is anything
+        if(hasData(0.1)){
+            int bullets = rand() % 10; // mock
+            while(!ic_push_bullet(bullets)){ // 
+                communicate(); // ensure that the buffer is cleared
+            }
+        }
+
+        // 2. Check for gamestate
+        state = ic_get_state();
+        if(state.packet_type == PACKET_DATA_GAMESTATE){
+            // process the state here
+            processGamestate(state);
+        }
+
+        // 3. Send MPU data at a constant rate
+        if(millis() - start_time >= delay){
+            MPUData data;
+            data.ax = rand() % 512;
+            data.ay = rand() % 512;
+            data.az = rand() % 512;
+            data.gx = rand() % 512;
+            data.gy = rand() % 512;
+            data.gz = rand() % 512;
+            while(!ic_push_imu(data)){
+                start_time = millis();
+                communicate(); // ensure that the buffer is cleared
+            }
+        }
+
+        // 4. Check for gamestate
+        state = ic_get_state();
+        if(state.packet_type == PACKET_DATA_GAMESTATE){
+            // process the state here
+            processGamestate(state);
+        }
     }
 
 }
