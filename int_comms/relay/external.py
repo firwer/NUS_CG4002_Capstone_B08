bd9c259894ce31adb1ce334d661683bffa6b0fb1
firwer <firwer@gmail.com>
2024-10-10 15:27:46 +0800

    feat: relay node integration

--- int_comms/relay/external.py
+++ int_comms/relay/external.py
@@ -1,11 +1,9 @@
 import asyncio
-import json
 import os
 import sys
 import time
 
-from sshtunnel import SSHTunnelForwarder
-
+from comms.AsyncMQTTController import AsyncMQTTController
 from ...ext_comms import config
 from ...ext_comms.comms import TCPC_Controller
 from ...ext_comms.comms import AsyncMQTTController as async_mqtt
@@ -13,20 +11,46 @@ from ...ext_comms.comms import AsyncMQTTController as async_mqtt
 from queue import Queue
 from threading import Thread
 
-
-async def user_input(send_queue: asyncio.Queue, receive_queue: asyncio.Queue, fromQueue: Queue):
-    while True:
-        if fromQueue.empty():
-            time.sleep(10)  # allow thread to be blocked to allow others to use. not strictly needed
-            continue
-        message = fromQueue.get()
-        if not message.startswith("p1_") and not message.startswith("p2_"):
-            print("Invalid message format. Must start with 'p1_' or 'p2_'")
-            continue
-        await send_queue.put(message)  # @WP TODO: is await really needed?
+RELAY_NODE_PLAYER = 1
 
 
-async def msg_receiver(wsController: TCPC_Controller, receive_queue: asyncio.Queue):
+async def send_to_server(fromBruno: Queue, toBruno: Queue, send_queue: Queue, receive_queue: Queue):
+    if RELAY_NODE_PLAYER == 1:
+        prefix = b'p1_'
+    elif RELAY_NODE_PLAYER == 2:
+        prefix = b'p2_'
+    else:
+        raise ValueError('Invalid RELAY_NODE_PLAYER value. Must be 1 or 2.')
+
+    loop = asyncio.get_running_loop()
+
+    async def get_from_thread_queue(q):
+        return await loop.run_in_executor(None, q.get)
+
+    async def put_to_thread_queue(q, item):
+        await loop.run_in_executor(None, q.put, item)
+
+    async def process_fromBruno():
+        while True:
+            data = await get_from_thread_queue(fromBruno)
+            message = prefix + data
+            await send_queue.put(message)
+
+    async def process_receive_queue():
+        while True:
+            data = await receive_queue.get()
+            await put_to_thread_queue(toBruno, data)
+
+    await asyncio.gather(
+        process_fromBruno(),
+        process_receive_queue()
+    )
+"""
+ Functions to handle direct communication between the queue and the TCP server
+"""
+
+
+async def receive_queue_handler(wsController: TCPC_Controller, receive_queue: Queue):
     """
     Continuously receive messages from the TCP server and place them in the queue.
     """
@@ -40,79 +64,67 @@ async def msg_receiver(wsController: TCPC_Controller, receive_queue: asyncio.Que
             continue  # After reconnection, continue receiving
 
 
-async def msg_sender(wsController: TCPC_Controller, send_queue: asyncio.Queue):
+async def send_queue_handler(wsController: TCPC_Controller, send_queue: Queue):
     """
-    Send messages from the relay node to the TCP server.
+    Continuously get messages in queue and send to the TCP server.
     """
     while True:
         message = await send_queue.get()
+
+        message.decodd
+
         await wsController.send(message)
 
 
-async def run_tcp_client(send_queue: asyncio.Queue, receive_queue: asyncio.Queue, port: int, fromQueue: Queue):
-    """
-    Establish a connection to the TCP server and handle reconnections.
-    """
-    wsController = TCPC_Controller(config.TCP_SERVER_HOST, port, config.TCP_SECRET_KEY)
-
+async def async_thread_main(fromBlunos: Queue, toBlunos: Queue, sendToGameServerQueue: Queue,
+                            receiveFromGameServerQueue: Queue):
+    # Establish a connection to the TCP server and handle reconnections.
+    wsController = TCPC_Controller(config.TCP_SERVER_HOST, config.TCP_SERVER_PORT, config.TCP_SECRET_KEY)
     await wsController.connect()
 
+    # Async queues are used for communication between the TCP server and the relay node
     # Tasks for sending and receiving messages
     tasks = [
-        asyncio.create_task(msg_sender(wsController, send_queue)),
-        asyncio.create_task(msg_receiver(wsController, receive_queue)),
-        asyncio.create_task(user_input(send_queue, receive_queue, fromQueue))
+        asyncio.create_task(receive_queue_handler(wsController, receiveFromGameServerQueue)),
+        asyncio.create_task(send_queue_handler(wsController, sendToGameServerQueue)),
+        asyncio.create_task(send_to_server(fromBlunos, toBlunos, sendToGameServerQueue, receiveFromGameServerQueue))
     ]
 
     await asyncio.gather(*tasks)  # Ensure both sender and receiver tasks run concurrently
 
 
-async def ext_main(fromBlunos, toBlunos):
-    send_queue = asyncio.Queue()
-    receive_queue = asyncio.Queue()
-    await run_tcp_client(send_queue, receive_queue, config.TCP_SERVER_PORT)
-
-
-def aggregator_thread(fromQueue: Queue, from1: Queue, from2: Queue, from3: Queue, to1: Queue, to2: Queue,
-                      MQTT_receive: Queue):
+def aggregator_thread_main(sendToGameServerQueue: Queue, from1: Queue, from2: Queue, from3: Queue, to1: Queue, to2: Queue,
+                           receiveFromGameServerQueue: Queue):
     """Aggregates the beetles' data"""
     # round robin executor
     while True:
         if not from1.empty():
-            fromQueue.put(from1.get())
+            sendToGameServerQueue.put(from1.get())
         if not from2.empty():
-            fromQueue.put(from2.get())
+            sendToGameServerQueue.put(from2.get())
         if not from3.empty():
-            fromQueue.put(from3.get())
-        if not MQTT_receive.empty():
-            data = MQTT_receive.get()
+            sendToGameServerQueue.put(from3.get())
+        if not receiveFromGameServerQueue.empty():
+            # Received data from the game server, propagate latest game states to the beetles
+            data = receiveFromGameServerQueue.get()
             to1.put(data)
             to2.put(data)
 
-
-def mqtt_thread(MQTT_receive: Queue):
-    # @WP TODO: Create a connection to the broker, receive data and send to MQTT_receive
-    foo = asyncio.Queue()
-    bar = asyncio.Queue()
-    rx = asyncio.Queue()
-    async_mqtt.AsyncMQTTController(config.MQTT_BROKER_PORT, foo, bar, rx)
+        #busy wait issue fixme
 
 
-def entry_thread(from1: Queue, from2: Queue, from3: Queue, to1: Queue, to2: Queue):
+def entry_thread(fromBeetle1: Queue, fromBeetle2: Queue, fromBeetle3: Queue, toBeetle1: Queue, toBeetle2: Queue):
+    receiveFromGameServerQueue = Queue()
+    sendToGameServerQueue = Queue()
     # This thread should be run from main()
-    fromQueue = Queue()
-    MQTT_receive = Queue()
-    agg_thread = Thread(target=aggregator_thread, args=(fromQueue, from1, from2, from3, to1, to2, MQTT_receive))
-    agg_thread.run()
-    # @WP TODO: we will launch the MQTT thread here
-    asyncio.run(ext_main(from1, from2, from3, to1, to2))
+    # Start the aggregator thread
+    agg_thread = Thread(target=aggregator_thread_main, args=(
+        sendToGameServerQueue, fromBeetle1, fromBeetle2, fromBeetle3, toBeetle1, toBeetle2, receiveFromGameServerQueue))
+    agg_thread.start()
+
+    # Start the async thread
+    async_thread = Thread(target=async_thread_main, args=(sendToGameServerQueue, receiveFromGameServerQueue))
+    async_thread.start()
+
     agg_thread.join()
-
-
-if sys.platform.lower() == "win32" or os.name.lower() == "nt":
-    from asyncio import set_event_loop_policy, WindowsSelectorEventLoopPolicy
-
-    set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-
-if __name__ == "__main__":
-    asyncio.run(ext_main())
+    async_thread.join()
