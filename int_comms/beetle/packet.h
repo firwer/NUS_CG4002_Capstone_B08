5b30c6bb097ebe5e03714f5bbed82b53e649de24
nichyjt <nicholas.yek@gmail.com>
2024-10-28 11:21:03 +0800

    add shield cnt to beetle ic packet struct

--- int_comms/beetle/packet.h
+++ int_comms/beetle/packet.h
@@ -57,8 +57,11 @@ typedef struct packet_health_t {
     uint8_t packet_type;
     uint8_t seq_num;
     uint8_t health_count;
+    uint8_t shield_count;
     uint64_t pad0;
-    uint64_t pad1;
+    uint32_t pad1;
+    uint16_t pad2;
+    uint8_t pad3;
     uint8_t crc8;
 };
 
