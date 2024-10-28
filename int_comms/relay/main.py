41c1b02a832685858f2b00082cf9ba99c6a86736
Wei Pin <firwer@gmail.com>
2024-10-28 16:08:31 +0800

    fix: some stuff

--- int_comms/relay/main.py
+++ int_comms/relay/main.py
@@ -407,7 +407,7 @@ def main():
         beetle0 = Beetle(BLUNO_P1_GLOVE_MAC, 0, sendToGameServerQueue, receiveFromGameServerQueue0)
         #beetle1 = Beetle(BLUNO_P1_CHEST_MAC, 1, sendToGameServerQueue, receiveFromGameServerQueue1)
         #beetle2 = Beetle(BLUNO_P2_LEG_MAC, 2, sendToGameServerQueue)
-        externalThread = threading.Thread(target=external.begin_external, args=(sendToGameServerQueue, receiveFromGameServerQueue0, receiveFromGameServerQueue1, 1,), name="External")
+        externalThread = threading.Thread(target=external_p1.begin_external, args=(sendToGameServerQueue, receiveFromGameServerQueue0, receiveFromGameServerQueue1, 1,), name="External")
         t0 = threading.Thread(target=run_beetle, args=(beetle0,), name="Beetle0")
         #t1 = threading.Thread(target=run_beetle, args=(beetle1,), name=f"Beetle1")
         #t2 = threading.Thread(target=run_beetle, args=(beetle2,), name=f"Beetle2")
